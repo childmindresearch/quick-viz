02601880e0f2c5c40b2e49c620dfc5bedc4b362e
gkiar <gkiar@precision>
2022-10-06 13:32:26 -0400

    added parameter descriptions

--- code/plot_tsv_heatmap.py
+++ code/plot_tsv_heatmap.py
@@ -1,15 +1,24 @@
 #!/usr/bin/env python
 
+"""
+Example usage:
+
+python plot_tsv_heatmap.py /path/to/my/data.tsv /path/to/my/data_tsv_image.png
+
+"""
+
 from argparse import ArgumentParser
 import matplotlib.pyplot as plt
 import numpy as np
 
 
-
 def main():
-    parser = ArgumentParser()
-    parser.add_argument('in_tsv')
-    parser.add_argument('plot_loc')
+    parser = ArgumentParser("Produce a heatmap from a TSV")
+    parser.add_argument('in_tsv', help="TSV data to visualize")
+    parser.add_argument('plot_loc', help="Target location for plot")
+    # TODO:
+    #  - accept delimiter option
+    #  - accept scaling options
 
     results = parser.parse_args()
 
