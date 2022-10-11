697ea75e0c97b039822a61b566d6f3d37f9028f4
gkiar <gkiar@something>
2022-10-11 21:23:32 +0000

    added option to specify subvolume

--- code/plot_nii_overlay.py
+++ code/plot_nii_overlay.py
@@ -5,7 +5,8 @@ Example usage:
 
 python plot_nii_overlay.py /path/to/my/data.nii.gz \
                            /path/to/my/image.png \
-                           -b /path/to/reference/data.nii.gz
+                           -b /path/to/reference/data.nii.gz \
+                           -v 1
 
 """
 
@@ -21,6 +22,8 @@ def main():
     parser.add_argument('in_nii', help="3D Nifti data to visualize")
     parser.add_argument('plot_loc', help="Target location for plot")
     parser.add_argument('-b', '--background', help="Background (reference) image")
+    parser.add_argument('-v', '--volume', type=int,
+                        help="Single 3D volume from a 4D stack to show")
 
 
     # TODO:
@@ -30,7 +33,13 @@ def main():
     results = parser.parse_args()
 
     im = nib.load(results.in_nii)
+    if results.volume is not None:
+        v = results.volume
+        im = nib.Nifti1Image(im.get_fdata()[:,:,:,v], header=im.header, affine=im.affine)
+
+    # Note: this threshold is arbitrary
     lb = np.percentile(np.sort(np.unique(im.get_fdata())), 20)
+
     if results.background:
         bg = nib.load(results.background)
         plot_stat_map(im, bg_img = bg, output_file=results.plot_loc,
