7c78ae92672d293944e4415725e63465974e2736
e-kenneally <113037677+e-kenneally@users.noreply.github.com>
2022-10-12 15:58:10 -0400

    Update surface_viz.py

--- code/surface_viz.py
+++ code/surface_viz.py
@@ -1,12 +1,14 @@
+""" 
+
+Example usage:
+python surface_viz.py /path/to/img.gii
+
+"""
+
 import nilearn
 import sys
 import matplotlib
-from nilearn import plotting, datasets
-
-""" 
-Example usage:
-python surface_viz.py /path/to/img.gii
-"""
+from nilearn import plotting
 
 def main():
 
@@ -14,7 +16,6 @@ def main():
   img = sys.argv[1]
   fig = nilearn.plotting.plot_surf(img)
   fig.figure.show()
-
 
   
 if __name__ == "__main__":
